e802e133024e13b6a56717fad83852865c526286
SaiRupaJ <143540330+SaiRupaJ@users.noreply.github.com>
2023-10-03 18:38:49 -0400

    Assignment-5

--- assignments/NearestNeighbor/my_KNN.py
+++ assignments/NearestNeighbor/my_KNN.py
@@ -1,3 +1,4 @@
+from turtle import distance
 import pandas as pd
 import numpy as np
 from collections import Counter
@@ -12,26 +13,48 @@ class my_KNN:
         self.n_neighbors = int(n_neighbors)
         self.metric = metric
         self.p = p
+    
+    def _distance(self, x1, x2):
+        if self.metric == "minkowski":
+            return np.linalg.norm(np.array(x1) - np.array(x2), ord=self.p)
+        elif self.metric == "manhattan":
+            return np.sum(np.abs(np.array(x1) - np.array(x2)))
+        elif self.metric == "euclidean":
+            return np.linalg.norm(np.array(x1) - np.array(x2))
+        elif self.metric == "cosine":
+            return 1 - np.dot(x1, x2) / (np.linalg.norm(x1) * np.linalg.norm(x2))
+
+
 
     def fit(self, X, y):
         # X: pd.DataFrame, independent variables, float
+        self.X_train=X
+        self.y_train=y
         # y: list, np.array or pd.Series, dependent variables, int or str
         self.classes_ = list(set(list(y)))
-        # write your code below
-        return
+        
 
     def predict(self, X):
-        # X: pd.DataFrame, independent variables, float
-        # return predictions: list
-        # write your code below
+        predictions = []
+        for x in X.values: 
+            # Iterate through rows of X as numpy arrays
+            K_distances = [self._distance(x, x_train) for x_train in self.X_train.values]
+            I = np.argsort(K_distances)
+            K_nearest_indices = I[:self.n_neighbors]
+            K_nearest_labels = [self.y_train.iloc[idx] for idx in K_nearest_indices]
+            most_common = Counter(K_nearest_labels).most_common(1)
+            predictions.append(most_common[0][0])
         return predictions
 
+
     def predict_proba(self, X):
-        # X: pd.DataFrame, independent variables, float
-        # prob is a dict of prediction probabilities belonging to each categories
-        # return probs = pd.DataFrame(list of prob, columns = self.classes_)
-        # write your code below
-        return probs
-
-
-
+        probs = []
+        for i in X.values:
+            K_distances = [self._distance(i, i_train) for i_train in self.X_train.values]
+            I = np.argsort(K_distances)
+            K_nearest_indices = I[:self.n_neighbors]
+            K_nearest_labels = [self.y_train.iloc[idx] for idx in K_nearest_indices]
+            No_of_counts = Counter(K_nearest_labels)
+            prob_dict = {cls: No_of_counts[cls] / self.n_neighbors for cls in self.classes_}
+            probs.append(prob_dict)
+        return pd.DataFrame(probs, columns=self.classes_)
